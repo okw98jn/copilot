57ddcf4c0cf15a1cc16bf20830b82dc90bbf2a81
okw98jn <lcmc9vgool@gmail.com>
2025-05-31 10:47:10 +0900

    feat: UserModel構造体を新規作成
    ユーザーモデルを定義するUserModel構造体を追加しました。これにより、ユーザー情報の管理が可能になります。フィールドにはID、名前、メールアドレス、作成日時、更新日時が含まれています。

---
+++ src/internal/infrastructure/db/model/user_model.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type UserModel struct {
+	bun.BaseModel `bun:"table:users,alias:u"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Name      string    `bun:"name,notnull"`
+	Email     string    `bun:"email,unique,notnull"`
+	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp"`
+}
